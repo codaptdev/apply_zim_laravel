5d07398f73317073d26edb2bb4509aa828c87eb7
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-07 00:05:47 +0200

    renamed component

--- app/View/Components/main-layout.php
+++ app/View/Components/main-layout.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class main-layout extends Component
+class mainLayout extends Component
 {
     /**
      * Create a new component instance.
