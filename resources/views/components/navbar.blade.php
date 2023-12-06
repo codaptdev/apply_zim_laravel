ab9fd29b71d179da9ede5f9d2aafe3644427704f
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-06 15:54:03 +0200

    adds main layout and navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,19 @@
+<div class=" flex flex-row items-center justify-start border-b-2 mb-5 w-full p-5 " >
+
+    {{-- Logo --}}
+
+    <a class="text-3xl w-1/5 font-bold text-black hover:text-indigo-500 cursor-pointer font-serif transition-all " >ApplyZim</a>
+
+    {{-- Nav Menu --}}
+    <div class=" flex flex-row justify-center items-center text-xl font-bold  gap-1 w-full" >
+        <a class="hover:text-indigo-500 mx-2 " href="">Home</a>
+        <a class="hover:text-indigo-500 mx-2 " href="">About</a>
+        <a class="hover:text-indigo-500 mx-2 " href="">Register</a>
+    </div>
+
+    {{-- Action Button --}}
+    <div class="w-1/5 flex flex-col justify-end items-center " >
+        <a href="" class="bg-indigo-500 drop-shadow-md px-9 py-3 rounded-xl text-white  " >Sign In</a>
+    </div>
+
+</div>
