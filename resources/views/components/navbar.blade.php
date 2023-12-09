c69ef4330d3cd6a97f8b7e3a075fd2627540efdc
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-09 23:01:14 +0200

    nakes navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,48 +1,73 @@
 <div class=" flex flex-row items-center justify-start border-b-2 mb-5 w-full p-5 " >
 
-    {{-- Logo --}}
+    {{-- Hamburger on small screens --}}
+    <div class="md:hidden flex flex-row w-full justify-start items-center" >
 
-    <a href="/" class="text-3xl w-1/5 font-bold text-black hover:text-indigo-500 cursor-pointer font-serif transition-all " >ApplyZim</a>
+        {{-- Logo --}}
+        <a href="/" class="text-3xl w-4/5 font-bold text-black hover:text-indigo-500 cursor-pointer font-serif transition-all " >ApplyZim</a>
 
-    @guest
+        {{-- Link to full menu --}}
+        <a href="/menu" class="w-full flex justify-end items-center " >
+            <div class="flex flex-col w-1/4 h-full">
+                <div class="bg-black rounded-full shadow-xl w-full h-2 m-1"></div>
+                <div class="bg-black rounded-full shadow-xl w-full h-2 m-1"></div>
+                <div class="bg-black rounded-full shadow-xl w-full h-2 m-1"></div>
+            </div>
+        </a>
 
-
-    {{-- Nav Menu --}}
-    <div class=" flex flex-row justify-center items-center text-xl font-bold  gap-1 w-full" >
-        <a class="hover:text-indigo-500 mx-2 " href="/">Home</a>
-        <a class="hover:text-indigo-500 mx-2 " href="/about">About</a>
-        <a href="/register" class="hover:text-indigo-500 mx-2 " href="/register">Register</a>
     </div>
 
-    {{-- Action Button --}}
-    <div class="w-1/5 flex flex-col justify-end items-center " >
-        <a href="/auth/signin" class="bg-indigo-500 drop-shadow-md px-9 py-3 rounded-xl text-white  " >Sign In</a>
-    </div>
+    {{-- Full menu bigger screen --}}
+    <div class="md:flex-row items-center justify-start w-full hidden md:flex" >
 
-    @else
-        {{-- Nav Menu --}}
-        @if (auth()->user()->user_type == "STUDENT")
-        <div class=" flex flex-row justify-center text-black items-center text-xl font-bold  gap-1 w-full" >
-            <a class="hover:text-indigo-500 mx-2 text-black" href="/home">Home</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/search">Search</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/profile">Applications</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/settings">Settings</a>
-        </div>
+
+        {{-- Logo --}}
+
+        <a href="/" class="text-3xl w-1/5 font-bold text-black hover:text-indigo-500 cursor-pointer font-serif transition-all " >ApplyZim</a>
+
+        @guest
+
+            {{-- Nav Menu for UnAuthenticated Users --}}
+            <div class=" flex flex-row justify-center items-center text-xl font-bold  gap-1 w-full" >
+                <a class="hover:text-indigo-500 mx-2 " href="/">Home</a>
+                <a class="hover:text-indigo-500 mx-2 " href="/about">About</a>
+                <a href="/register" class="hover:text-indigo-500 mx-2 " href="/register">Register</a>
+            </div>
+
+            {{-- Action Button --}}
+            <div class="w-1/5 flex flex-col justify-end items-center " >
+                <a href="/auth/signin" class="bg-indigo-500 drop-shadow-md px-9 py-3 rounded-xl text-white  " >Sign In</a>
+            </div>
+
         @else
-        <div class=" flex flex-row justify-center text-black items-center text-xl font-bold  gap-1 w-full" >
-            <a class="hover:text-indigo-500 mx-2 text-black" href="/home">Home</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/applications">Applications</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/myschool">My School</a>
-            <a class="hover:text-indigo-500 mx-2 text-black " href="/settings">Settings</a>
-        </div>
-        @endif
 
+            {{-- Nav Menu --}}
+            @if (auth()->user()->user_type == "STUDENT")
 
+                {{-- Nav Menu for Students --}}
+                <div class=" flex flex-row justify-center text-black items-center text-xl font-bold  gap-1 w-full" >
+                    <a class="hover:text-indigo-500 mx-2 text-black" href="/home">Home</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/search">Search</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/profile">Applications</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/settings">Settings</a>
+                </div>
+            @else
+                {{-- Full Nav for Schools --}}
+                <div class=" flex flex-row justify-center text-black items-center text-xl font-bold  gap-1 w-full" >
+                    <a class="hover:text-indigo-500 mx-2 text-black" href="/home">Home</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/applications">Applications</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/myschool">My School</a>
+                    <a class="hover:text-indigo-500 mx-2 text-black " href="/settings">Settings</a>
+                </div>
+            @endif
 
-    {{-- Action Button --}}
-    <div class="w-1/5 flex flex-col justify-end items-center " >
-        <a href="/auth/signout" class="bg-indigo-500 drop-shadow-md px-9 py-3 rounded-xl text-white  " >Sign Out</a>
+            {{-- Action Button --}}
+            <div class="w-1/5 flex flex-col justify-end items-center " >
+                <a href="/auth/signout" class="bg-indigo-500 drop-shadow-md px-9 py-3 rounded-xl text-white  " >Sign Out</a>
+            </div>
+
+        @endguest
+
     </div>
-    @endguest
 
 </div>
