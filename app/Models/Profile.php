255ccf6a262889caf1c81d4d5f3010190d7abb9e
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-10 23:51:51 +0200

    adds fillable methods

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,6 +9,22 @@ class Profile extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'school_id',
+        'about',
+        'body',
+        'webiste_url',
+        'application_url',
+        'instagram',
+        'twitter',
+        'facebook',
+        'banner_url',
+        'application_process',
+        'tuition_max',
+        'tuition_min',
+
+    ];
+
     public function school() {
         return $this->belongsTo(School::class);
     }
