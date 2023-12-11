bec17320bd850cf88c8806e0d4ae6b5e01358814
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-12 00:36:08 +0200

    adds error and notice messages

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -3,3 +3,15 @@
         <p>{{session('message')}}</p>
     </div>
 @endif
+
+@if (session()->has('error'))
+    <div class="p-5 w-full bg-red-400 bottom-0 left-0 fixed text-white">
+        <p>{{session('error')}}</p>
+    </div>
+@endif
+
+@if (session()->has('notice'))
+    <div class="p-5 w-full bg-orange-400 bottom-0 left-0 fixed text-white">
+        <p>{{session('notice')}}</p>
+    </div>
+@endif
