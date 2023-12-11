95ae849df16492bd61a331e2cde1034086b624d9
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-12-12 00:37:27 +0200

    adds views to get applications

--- resources/views/students/applications.blade.php
+++ resources/views/students/applications.blade.php
@@ -1,41 +1,52 @@
 <x-main-layout>
     <div class="flex p-10 flex-col justify-center items-center ">
-        <h1>Your Applications Attempts History</h1>
+        <h1 class="text-center" >Your Applications Attempts History</h1>
 
-        <p class="mt-5 text-slate-600 font-medium text-lg" >This is a list of schools that you have attempted to apply to by pressing the <strong>Apply button</strong> on
+        <p class="mt-5 text-center text-slate-600 font-medium text-md" >This is a list of schools that you have attempted to apply to by pressing the <strong>Apply button</strong> on
             their school profile page. This doesn't neccessarily mean that you applied for this schools but you clicked on
             <strong>Apply Button</strong>
         </p>
 
         <br>
 
-        <table >
-            {{-- Head --}}
-            <thead >
-                <tr>
-                    <th>School</th>
-                    <th>Level</th>
-                    <th>Date Applied</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
 
-            <tbody>
+        @if (empty($schools))
+            <div class="w-full flex flex-col items-center justify-center text-center p-20">
+                <p class="text-slate-500 text-xl " >You have not Applied to any schools yet🤭</p>
+                <a class=" text-xl " href="/search" >Search Schools</a>
+            </div>
+        @else
+            <table>
+                {{-- Head --}}
+                <thead >
+                    <tr>
+                        <th>School</th>
+                        <th>Application Url</th>
+                        {{-- <th>Date Applied</th> --}}
+                        <th>Action</th>
+                    </tr>
+                </thead>
 
-                {{-- Data --}}
-                @foreach ($schools as $key => $school)
-                <tr  >
-                    <td class="hover:bg-slate-200" > <a href="/schools/{{$school->id}}">{{$school->name}}</a></td>
-                    <td class="hover:bg-slate-200" >{{$school->level}}</td>
-                    <td class="hover:bg-slate-200" >{{$applications[$key]->created_at}}</td>
-                    <td class="hover:bg-red-200 hover:text-red-800 bg-red-400" >
-                        <a href="/application/delete/{{$school->id}}">Delete</a>
-                    </td>
-                </tr>
-                @endforeach
+                <tbody>
 
-            </tbody>
-        </table>
+
+                    {{-- Data --}}
+                    @foreach ($schools as $key => $school)
+                    <tr  >
+                        <td class="hover:bg-slate-200" > <a href="/schools/{{$school->id}}">{{$school->name}}</a></td>
+                        <td class="hover:bg-slate-200"> <a href="{{$school->application_url}}">{{$school->application_url}}</a></td>
+                        <td class="hover:bg-slate-200" >{{$applications[$key]->created_at}}</td>
+                        <td class="hover:bg-red-200 hover:text-red-800 bg-red-400">
+                            <a href="/applications/delete/{{$school->id}}">Delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        @endif
+
+
 
     </div>
 </x-main-layout>
