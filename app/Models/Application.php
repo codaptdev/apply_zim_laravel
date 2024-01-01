79ed40e83ca84dc57d598b6ae788630fb1de5cab
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-01-01 18:39:56 +0200

    adds static methods, find and exists to application model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -18,4 +18,40 @@ class Application extends Model
     public function school() {
         return $this->belongsTo(School::class);
     }
+
+    /**
+     * Takes positional arguements `school_id` and a `student_id`
+     *
+     * Return true if an application already exists.
+     *
+     * Returns false if the the application doesn't exists
+     */
+    public static function exists(int $school_id, int $student_id) {
+
+        $applications = static::all()
+        ->where('student_id', $student_id)
+        ->where('school_id', $school_id);
+
+        return $applications->count() > 0;
+    }
+
+    /** Get a specific application by the student's id and school's id */
+    public static function find(int $school_id, int $student_id) {
+        $application = static::all()
+        ->where('student_id', $student_id)
+        ->where('school_id', $school_id)
+        ->first();
+
+        return $application;
+    }
+
+    public static function schoolsAll(int $school_id) {
+        return static::all()
+        ->where('school_id', $school_id);
+    }
+
+    public static function studentsAll(int $student_id) {
+        return static::all()
+        ->where('student_id', $student_id);
+    }
 }
