6c5c6b58c153fa543c190f6cda159d11a82dd63b
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-01-02 20:22:35 +0200

    adds cards to stats page

---
+++ resources/views/components/stat-card.blade.php
@@ -0,0 +1,6 @@
+@props(['value', 'title'])
+
+<div class="w-full bg-slate-100 p-3  flex flex-col justify-center items-center rounded-xl h-full ">
+    <p class="font-medium text-slate-400 text-xl">{{$title}}</p>
+    <h1 class="text-slate-600" >{{$value}}</h1>
+</div>
---
+++ resources/views/stats/index.blade.php
@@ -0,0 +1,28 @@
+<x-main-layout>
+    <div class="flex p-10 flex-col justify-center items-center">
+        <h1>Statistics</h1>
+
+        <div class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1  my-10 w-full gap-3">
+
+            <x-stat-card
+                :title="'Profile Visits'"
+                :value="100"
+            />
+
+            <x-stat-card
+                :title="'Times Bookmarked'"
+                :value="26"
+            />
+
+            <x-stat-card
+                :title="'Attempts to Apply'"
+                :value="123"
+            />
+
+            <x-stat-card
+                :title="'Redirects to your socials'"
+                :value="56"
+            />
+        </div>
+    </div>
+</x-main-layout>
