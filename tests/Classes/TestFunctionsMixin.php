d930d1d275a0067a07b620be3bb6872a059ff912
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-01-04 23:22:28 +0200

    adds test functions mixin

---
+++ tests/Classes/TestFunctionsMixin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Classes;
+
+use App\Models\User;
+use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
+
+/** Trait with useful reusable code for testing  */
+trait TestFunctionsMixin {
+    private function seedJohnDoeHelper() {
+        $user = new User([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' =>  '12345678',
+            'user_type' => 'STUDENT'
+        ]);
+
+        $user->save();
+
+        if($this->loginJohnDoe()) {
+            $student = new Student([
+                'user_id' => $user->id,
+                'first_name' => 'John',
+                'surname' => 'Doe',
+                'email' => 'john@example.com',
+                'level' => 'SECONDARY',
+                'town_city' => 'Harare'
+            ]);
+
+            $student->save();
+
+        } else {
+            print("💀 Couldn't create Student User John Doe");
+        }
+
+        return [
+            'user' => $user,
+            'student' => $student
+        ];
+    }
+
+    private function loginJohnDoe() : bool {
+        return (Auth::attempt(['email' => 'john@example.com', 'password' => '12345678']));
+    }
+}
