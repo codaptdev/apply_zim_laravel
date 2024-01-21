798169f006b38651a1c1ff3f7a0e375eb85b1610
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-01-21 18:58:11 +0200

    adds test for feature

--- tests/Feature/RedirectLogTest.php
+++ tests/Feature/RedirectLogTest.php
@@ -14,27 +14,30 @@ class RedirectLogTest extends TestCase
     /**
      * A basic feature test example.
      */
+
+    private $redirect_url = 'https://github.com/Tadiwr';
     public function test_if_redirect_works( ) {
 
         $school = $this->seedOneRandomSchoolAndLogin();
-        $url = 'https://github.com/Tadiwr';
+        $req_url = 'redirect?type=website&school_id=' . $school->id . '&url=' . $this->redirect_url;
 
-        $request = $this->get('/redirect?url=' . $url);
+        $request = $this->get($req_url);
 
-        $request->assertRedirect($url);
+        $request->assertRedirect($this->redirect_url);
 
         $this->logout();
     }
 
     public function test_if_redirect_log_saved () {
         $school = $this->seedOneRandomSchoolAndLogin();
-        $url = 'https://github.com/Tadiwr';
 
-        $request = $this->get('/redirect?url=' . $url);
+        $req_url = 'redirect?type=website&school_id=' . $school->id . '&url=' . $this->redirect_url;
+
+        $request = $this->get($req_url);
 
         $this->assertDatabaseHas('redirect_logs', [
             'school_id' => $school->id,
-            'to' => $url
+            'to' => $this->redirect_url
         ]);
 
         $this->logout();
