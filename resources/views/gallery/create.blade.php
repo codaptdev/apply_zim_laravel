e3a328824c444ae6dca7713d9ff282651a1b7c5f
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-16 00:04:02 +0200

    adds logic to send post request for a gallery item

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -1,3 +1,14 @@
 <x-main-layout>
-    <h1>Add Gallery Item</h1>
+
+    <div class="flex flex-col justify-center items-center p-10" >
+        <h1>Add Gallery Item</h1>
+
+        <form action="/gallery" method="post" class="flex flex-col items-center justify-center" enctype="multipart/form-data" >
+            @csrf
+            <input type="file" name="gallery_item" id="gallery_item"   >
+            <br>
+            <button type="submit" class="link-btn" >Upload</button>
+        </form>
+    </div>
+
 </x-main-layout>
