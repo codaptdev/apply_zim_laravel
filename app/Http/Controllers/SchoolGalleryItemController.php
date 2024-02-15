a06aa9d4ba64136eb5e30e15c198a6dc980e366c
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-15 22:55:57 +0200

    adds controller and web routes for gallery feature

--- app/Http/Controllers/SchoolGalleryItemController.php
+++ app/Http/Controllers/SchoolGalleryItemController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class SchoolGalleryItemController extends Controller
 {
-    //
+    // Get a grid of a schools gallery items
+    public function index() {
+        return view('gallery.index');
+    }
+
+    // Returns view to upload a gallery item
+    public function create() {
+        return view('gallery.create');
+    }
+
+    // Returns view to edit gallery items
+    public function edit() {
+        return view('gallery.edit');
+    }
 }
