c27988717ee6a804fedb2bb7c0f949c4bf643423
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-16 14:56:31 +0200

    adds ui elements to allow for gallery_items to be deleted

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -11,12 +11,4 @@
         </form>
     </div>
 
-   @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div class="alert alert-danger w-full p-5 bg-red-400">
-                <p class="text-white" >{{$error}}</p>
-            </div>
-        @endforeach
-   @endif
-
 </x-main-layout>
--- resources/views/gallery/edit.blade.php
+++ resources/views/gallery/edit.blade.php
@@ -6,7 +6,7 @@
         </div>
 
 
-        <div class="grid grid-cols-1 lg:grid-cols-3 w-full gap-3 px-10 h-56 justify-center items-center mb-10" >
+        <div class="grid grid-cols-1 w-full gap-3 px-10 h-56 justify-center items-center mb-10" >
 
             <a href="/gallery/create" class="w-full h-56 flex flex-col justify-center items-center p-5 bg-slate-100 hover:bg-slate-300 rounded-xl" >
                 <i class="fa-solid fa-add text-3xl  text-slate-900"></i>
@@ -14,8 +14,18 @@
             </a>
 
             @forelse ($gallery_items as $item)
-                <div class="h-56 w-full relative" >
-                    <img src="/storage/{{($item->url)}}" alt="" class="w-full rounded-xl z-10 h-56 object-cover hover:h-96 hover:absolute hover:z-20 transition-all">
+                <div class="flex md:flex-row flex-col" >
+                    <div class="h-40 w-1/4 relative" >
+                        <img src="/storage/{{($item->url)}}" alt="" class="w-full rounded-xl z-10 h-40 object-cover hover:h-96 hover:absolute hover:z-20 transition-all">
+                    </div>
+
+                    <div class="flex flex-row items-center p-10 justify-center ">
+
+                        <a class="text-red-500 hover:rounded-full hover:bg-red-100 p-3" href="/gallery/delete/{{$item->id}}">
+                            <i class="fa-solid fa-trash text-red-500 mr-2"></i>
+                            Delete From Gallery
+                        </a>
+                    </div>
                 </div>
             @empty
 
