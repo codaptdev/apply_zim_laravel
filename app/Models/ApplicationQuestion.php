1abe0acb2a8bc9bb6450e7a1ba48c2de9ea8fb45
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-19 11:02:51 +0200

    adds static methods to application questions model

--- app/Models/ApplicationQuestion.php
+++ app/Models/ApplicationQuestion.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ApplicationQuestion extends Model
 {
     use HasFactory;
+
+    /** Returns the number of questions for a schools form
+     * given a `schools id`
+     */
+    public static function count(int $school_id) : int {
+        $questions = static::all()
+        ->where('school_id', $school_id);
+
+        return $questions->count();
+    }
+
+    /** Get all the questions for a schools form given its `school id` */
+    public static function getSchoolsQuestions(int $school_id) {
+        return static::all()
+        ->where('school_id', $school_id);
+    }
 }
