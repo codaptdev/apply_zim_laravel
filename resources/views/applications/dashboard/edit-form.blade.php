356d5e1c94510aae621bad4ec605524b3710a7a6
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-19 12:07:49 +0200

    adds list of added questions to form edit view

--- resources/views/applications/dashboard/edit-form.blade.php
+++ resources/views/applications/dashboard/edit-form.blade.php
@@ -21,11 +21,15 @@
         </div>
 
         {{-- List of added questions --}}
-        <div id="form_body" class="flex flex-col items-center justify-center p-10" >
+        <div id="form_body" class="flex flex-col items-center justify-center p-10 w-3/4" >
             @forelse ($questions as $question)
-                <div>
-                    <p>{{$question->label}}</p>
-                    <p>{{$question->type}}</p>
+                <div class="flex flex-col p-5 m-3 rounded-xl bg-slate-100 w-full">
+                    <p class="w-full font-bold text-3xl" >{{$question->label}}</p>
+                    <p class="w-full text-slate-600 text-xl" >{{$question->placeholder}}</p>
+
+                    <div class="w-1/4 p-1" >
+                        <x-pill :href="''" :color="'blue'" >{{$question->response_type}}</x-pill>
+                    </div>
                 </div>
             @empty
                 <p class="text-slate-400 p-10 text-center" >Your Application form has no questions yet so Students won't be able to apply to your school through ApplyZim</p>
--- resources/views/components/pill.blade.php
+++ resources/views/components/pill.blade.php
@@ -1,18 +1,30 @@
 @props(['href', 'color'])
 
-<a
-    class="
-    bg-{{$color}}-200 border-2
-    border-{{$color}}-400
-    text-{{$color}}-800
-    py-2
-    px-4
-    rounded-full
-    hover:bg-{{$color}}-400
-    "
+
+
+@if ($href === "")
+
+    <p
+        class="
+        bg-{{$color}}-200 border-2 border-{{$color}}-400
+        text-{{$color}}-800 py-2 px-4
+        rounded-full hover:bg-{{$color}}-400 text-center"
+    >
+        {{-- The label put on the pill --}}
+        {{$slot}}
+    </p>
+
+
+@else
+    <a
+    class="bg-{{$color}}-200 border-2 border-{{$color}}-400
+    text-{{$color}}-800 py-2 px-4 rounded-fullhover:bg-{{$color}}-400 text-center"
 
     href="{{$href}}"
->
-    {{-- The lable put on the pill --}}
+    >
+    {{-- The label put on the pill --}}
     {{$slot}}
-</a>
+    </a>
+
+
+@endif
