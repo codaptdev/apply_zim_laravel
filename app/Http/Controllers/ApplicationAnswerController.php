7f77f9407f0a0d339dfe7277955c187803938307
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-19 12:51:06 +0200

    creates view to repond to application forms

--- app/Http/Controllers/ApplicationAnswerController.php
+++ app/Http/Controllers/ApplicationAnswerController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\School;
 use Illuminate\Http\Request;
 
 class ApplicationAnswerController extends Controller
 {
     /** returns a form for the student to fill out */
 
-    public function create() {
-        return view('applications.repond.form');
+    public function create(Request $request) {
+
+        $school = School::find($request->school_id);
+        $questions = $school->application_questions;
+
+        return view('applications.repond.form', compact('school', 'questions'));
     }
 }
--- resources/views/applications/repond/form.blade.php
+++ resources/views/applications/repond/form.blade.php
@@ -1,5 +1,23 @@
 <x-main-layout>
     <div class="flex flex-col justify-center items-center w-full">
-        <h1>Application Form</h1>
+        <h1>Apply to {{$school->name}}</h1>
+
+        <form action="/applications/forms/repond?school_id={{$school->id}}" class="p-10 mt-10 flex flex-col items-center justify-center" method="post">
+
+            {{-- Fields --}}
+
+            @foreach ($questions as $question)
+                <div class="flex flex-col items-start justify-center w-full pb-3" >
+                    <label class="py-3" >{{$question->label}}</label>
+                    <input type="{{strtolower($question->response_type)}}" name="{{$question->label}}" placeholder="{{$question->placeholder}}" class="border-solid w-full p-3 rounded-xl border-slate-400 border-2" type="text">
+                </div>
+            @endforeach
+
+            {{-- Submit button --}}
+
+            <button class="mt-5 link-btn" >Submit Application Form</button>
+
+        </form>
+
     </div>
 </x-main-layout>
