dd6f93a54f31cbc8f0c0ce0e83cae751ab6551da
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-02-19 11:03:40 +0200

    adds controller methods for edit and create

--- app/Http/Controllers/ApplicationQuestionController.php
+++ app/Http/Controllers/ApplicationQuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\School;
 use Illuminate\Http\Request;
+use App\Models\ApplicationQuestion;
 
 class ApplicationQuestionController extends Controller
 {
@@ -11,6 +13,19 @@ class ApplicationQuestionController extends Controller
     }
 
     public function edit() {
-        return view('applications.dashboard.edit-form');
+
+        // check if form exists
+
+        $school = School::withUserId(auth()->user()->id);
+
+        $questions = ApplicationQuestion::getSchoolsQuestions($school->id);
+
+        if ($questions->count() > 0) {
+            return view('applications.dashboard.edit-form', compact('questions', 'school'));
+        } else {
+            return redirect('/applications/dashboard/forms/create')
+            ->with('notice', 'You will need to create a form first before you can edit one');
+        }
+
     }
 }
